395792fd1a7f0eed755c92d6038fb5a87cbcceb3
juwoong kim <wwong7247@gmail.com>
2023-12-11 20:50:45 +0900

    feat : 프로젝트 기본환경 추가

---
+++ src/main/java/com/juwoong/reviewforme/ReviewForMeApplication.java
@@ -0,0 +1,13 @@
+package com.juwoong.reviewforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewForMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewForMeApplication.class, args);
+	}
+
+}
