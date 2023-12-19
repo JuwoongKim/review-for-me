9f653d6183c681c479f71c96945ff389009ec5db
juwoong kim <wwong7247@gmail.com>
2023-12-20 00:29:46 +0900

    feat: 질문 상속구조 제거

--- src/main/java/com/juwoong/reviewforme/domain/survey/domain/Question.java
+++ src/main/java/com/juwoong/reviewforme/domain/survey/domain/Question.java
@@ -11,16 +11,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name = "questions")
-public abstract class Question extends BaseEntity {
+public class Question extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
