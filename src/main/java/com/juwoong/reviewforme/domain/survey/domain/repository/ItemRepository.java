20dfe2f28a5e06eabaead218ecdacdaf8443cee9
juwoong kim <wwong7247@gmail.com>
2023-12-20 15:38:20 +0900

    feat: 질문 아이템 생성 기능을 위한 레포지토리 추가 및 서비스 기능 구현

--- src/main/java/com/juwoong/reviewforme/domain/survey/domain/repository/ItemRepository.java
+++ src/main/java/com/juwoong/reviewforme/domain/survey/domain/repository/ItemRepository.java
@@ -2,7 +2,7 @@ package com.juwoong.reviewforme.domain.survey.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.juwoong.reviewforme.domain.survey.domain.Survey;
+import com.juwoong.reviewforme.domain.survey.domain.item.Item;
 
-public interface SurveyRepository extends JpaRepository<Survey, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long> {
 }
